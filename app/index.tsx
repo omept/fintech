cbc10a91e6398591d38e345669015d8ac294522c
George Onwuasoanya <georgetheprogrammer@gmail.com>
2024-05-26 11:20:33 +0100

    - expo prebuild files - implemented home screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,66 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import Colors from '@/constants/Colors';
+import { defaultStyles } from '@/constants/Styles';
+import { useAssets } from 'expo-asset';
+import { ResizeMode, Video } from 'expo-av';
+import { Link } from 'expo-router';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-const Index = () => {
+const IndexPage = () => {
+  const [assets] = useAssets([require('@/assets/videos/intro2.mp4')]);
+
   return (
-    <View>
-      <Text>Home page</Text>
-    </View>
-  )
-}
+    <View style={styles.container}>
+      {assets && (
+        <Video
+          resizeMode={ResizeMode.COVER}
+          isMuted
+          isLooping
+          shouldPlay
+          source={{ uri: assets[0].uri }}
+          style={styles.video}
+        />
+      )}
+      <View style={{ marginTop: 80, padding: 20 }}>
+        <Text style={[styles.header, {color: "#000"}]}>Crypto To Fiat Exchange Info </Text>
+      </View>
 
-export default Index
+      <View style={styles.buttons}>
+        <Link
+          href={'/listing'}
+          style={[defaultStyles.pillButton, { flex: 1, backgroundColor: Colors.dark }]}
+          asChild>
+          <TouchableOpacity>
+            <Text style={{ color: 'white', fontSize: 22, fontWeight: '500' }}>View Listings</Text>
+          </TouchableOpacity>
+        </Link>
+        
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  video: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+  },
+  header: {
+    fontSize: 36,
+    fontWeight: '900',
+    textTransform: 'uppercase',
+    color: 'white',
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    gap: 20,
+    marginBottom: 60,
+    paddingHorizontal: 20,
+  },
+});
+export default IndexPage;
